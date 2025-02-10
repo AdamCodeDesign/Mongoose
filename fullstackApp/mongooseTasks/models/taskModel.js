4b98dcbd8a3ed27e73972e777fe5019a27092ff8
LeshchuDev <leshchudev@gmail.com>
2025-02-10 14:58:40 +0100

    task Maker added

--- fullstackApp/mongooseTasks/models/taskModel.js
+++ fullstackApp/mongooseTasks/models/taskModel.js
@@ -34,8 +34,34 @@ const taskSchema = mongoose.Schema({
 
 const Task = mongoose.model("Task", taskSchema);
 
-// const task1 = await Task.create({
-//     _id: new mongoose.Types.ObjectId(),
-//     title: "task #1",
-//     description: "zrob zadanie 1",
-// });
+function taskMaker(title, description) {
+    return new Task({
+        _id: new mongoose.Types.ObjectId(),
+        title,
+        description,
+    });
+}
+
+const tasksArr = [
+    taskMaker("task #1", "description to task #1"),
+    taskMaker("task #2", "description to task #2"),
+    taskMaker("task #3", "description to task #3"),
+    taskMaker("task #4", "description to task #4"),
+];
+
+try {
+    const tasksDb = await Task.find({});
+    console.log("Num of tasks in db: ", tasksDb.length);
+
+    if (tasksDb.length === 0) {
+        await Task.insertMany(tasksArr)
+            .then((result) => {
+                console.log("Tasks saved to db, num of tasks: ", result.length);
+            })
+            .catch((err) =>
+                console.error("Tasks NOT SAVED in db", err.message),
+            );
+    }
+} catch (error) {
+    console.log("error", error.message);
+}
