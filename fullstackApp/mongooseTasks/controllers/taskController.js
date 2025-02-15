d45d18ba5fe73e1577b9a6422258bec8bfda3357
LeshchuDev <leshchudev@gmail.com>
2025-02-15 14:49:55 +0100

    TaskController async functions added

--- fullstackApp/mongooseTasks/controllers/taskController.js
+++ fullstackApp/mongooseTasks/controllers/taskController.js
@@ -1,15 +1,29 @@
-import * as Task from "../models/taskModel.js"
+import * as Task from "../models/taskModel.js";
 
-export class TasksController{
-    constructor(){
-        this.init()
+export class TasksController {
+    constructor() {
+        this.init();
     }
 
-    async init(){
+    async init() {
         //this.tasks = await Task.getAll()
     }
 
-    async getAll(){
-        return await Task.getAll()
+    async getAll() {
+        return await Task.getAll();
+    }
+
+    async getById(id) {
+        return await Task.getById(id);
+    }
+
+    async deleteById(id) {
+        const result = await Task.deleteById(id);
+
+        if (result && result.deletedCount > 0) {
+            return { deleted: true, deletedCount: result.deletedCount };
+        } else {
+            return { deleted: false, deletedCount: 0 };
+        }
     }
 }
